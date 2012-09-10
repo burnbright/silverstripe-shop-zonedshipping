7dfb3b98a68a37dae7cb961ad8a884605ea119d1
Jeremy Shipman <jeremy@burnbright.net>
2012-09-10 16:28:11 +1200

    NEW: Add menu priority to ShippingAdmin

--- code/ShippingAdmin.php
+++ code/ShippingAdmin.php
@@ -4,6 +4,7 @@ class ShippingAdmin extends ModelAdmin{
 	
 	static $url_segment = "shipping";
 	static $menu_title = "Shipping";
+	static $menu_priority = 6;
 	
 	static $managed_models = array(
 		'ShippingZone'
